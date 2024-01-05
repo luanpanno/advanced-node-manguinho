a8d7fcaf64110448b97667b063e29461d7d5914e
Luan Panno <luanpanno@gmail.com>
2024-01-04 22:50:17 -0300

    Ensure AccessToken expires in 30 minutes

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -1,3 +1,11 @@
 export class AccessToken {
   constructor(private readonly value: string) {}
+
+  static get expirationInMs(): number {
+    const timeInMinutes = 30;
+    const seconds = 60;
+    const ms = 1000;
+
+    return timeInMinutes * seconds * ms;
+  }
 }
