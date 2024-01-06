0f919c01893ec5605b3c698e1fef3da39507bbf2
Luan Panno <luanpanno@gmail.com>
2024-01-05 21:57:05 -0300

    Move production code to their own files

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -1,39 +1,7 @@
 import { mock } from 'jest-mock-extended';
 
-import { LoadFacebookUserApi } from '@/data/contracts/apis';
-
-class FacebookApi {
-  private readonly baseUrl = 'https://graph.facebook.com';
-
-  constructor(
-    private readonly httpClient: HttpGetClient,
-    private readonly clientId: string,
-    private readonly clientSecret: string,
-  ) {}
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async loadUser(params: LoadFacebookUserApi.Params): Promise<void> {
-    this.httpClient.get({
-      url: `${this.baseUrl}/oauth/access_token`,
-      params: {
-        client_id: this.clientId,
-        client_secret: this.clientSecret,
-        grant_type: 'client_credentials',
-      },
-    });
-  }
-}
-
-interface HttpGetClient {
-  get: (params: HttpGetClient.Params) => Promise<void>;
-}
-
-namespace HttpGetClient {
-  export type Params = {
-    url: string;
-    params: object;
-  };
-}
+import { FacebookApi } from '@/infra/apis/facebook';
+import { HttpGetClient } from '@/infra/http/client';
 
 const makeSut = () => {
   const clientId = 'any_client_id';
