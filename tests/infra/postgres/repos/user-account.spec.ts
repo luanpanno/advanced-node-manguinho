7047c514ce26f4ed7363d6cc1db0f92c4322520c
Luan Panno <luanpanno@gmail.com>
2024-01-10 22:44:27 -0300

    Ensure PgUserAccountRepository loads account if email exists

---
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -0,0 +1,65 @@
+import { newDb } from 'pg-mem';
+import { Column, Entity, PrimaryGeneratedColumn, getRepository } from 'typeorm';
+
+import { LoadUserAccountRepository } from '@/data/contracts/repos';
+
+@Entity({ name: 'usuarios' })
+class PgUser {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ nullable: true, name: 'nome' })
+  name?: string;
+
+  @Column()
+  email!: string;
+
+  @Column({ nullable: true, name: 'id_facebook' })
+  facebookId?: string;
+}
+
+class PgUserAccountRepository implements LoadUserAccountRepository {
+  async load(
+    params: LoadUserAccountRepository.Params,
+  ): Promise<LoadUserAccountRepository.Result> {
+    const pgUserRepo = getRepository(PgUser);
+    const pgUser = await pgUserRepo.findOne({ email: params.email });
+
+    return {
+      id: pgUser!.id.toString(),
+      name: pgUser?.name ?? undefined,
+    };
+  }
+}
+
+const makeSut = () => {
+  const sut = new PgUserAccountRepository();
+
+  return {
+    sut,
+  };
+};
+
+describe('PgUserAccountRepository', () => {
+  describe('load', () => {
+    it('should return an account if email exists', async () => {
+      const db = newDb();
+      const connection = await db.adapters.createTypeormConnection({
+        type: 'postgres',
+        entities: [PgUser],
+      });
+
+      await connection.synchronize();
+
+      const pgUserRepo = getRepository(PgUser);
+
+      await pgUserRepo.save({ email: 'existing_email' });
+
+      const { sut } = makeSut();
+
+      const account = await sut.load({ email: 'existing_email' });
+
+      expect(account).toEqual({ id: '1' });
+    });
+  });
+});
