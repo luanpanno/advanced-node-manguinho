281abab91875e3b450b2073651992815c7355f04
Luan Panno <luanpanno@gmail.com>
2024-01-16 21:48:51 -0300

    Move production code to their own files

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -1,22 +1,7 @@
 import { MockProxy, mock } from 'jest-mock-extended';
 
-interface Validator {
-  validate: () => Error | undefined;
-}
-
-class ValidationComposite implements Validator {
-  constructor(private readonly validators: Validator[]) {}
-
-  validate(): Error | undefined {
-    for (const validator of this.validators) {
-      const error = validator.validate();
-
-      if (error !== undefined) {
-        return error;
-      }
-    }
-  }
-}
+import { ValidationComposite } from '@/application/validation/composite';
+import { Validator } from '@/application/validation/validator';
 
 describe('ValidationComposite', () => {
   let sut: ValidationComposite;
