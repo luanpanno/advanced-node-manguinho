56d0d46bf83c501124d9b55996942829cf4336f7
Luan Panno <luanpanno@gmail.com>
2024-01-17 20:13:15 -0300

    Add jest config for integration tests

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,7 @@ const config = {
     '@/tests/(.+)': '<rootDir>/tests/$1',
     '@/(.+)': '<rootDir>/src/$1',
   },
+  testMatch: ['**/*.spec.ts'],
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   transform: {
     '\\.ts$': 'ts-jest',
