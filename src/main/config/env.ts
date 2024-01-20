6f0dc68c37760a7a9b82e5234e670aeabc4cb7e0
Luan Panno <luanpanno@gmail.com>
2024-01-20 02:02:42 -0300

    Move typeorm config to root folder

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -9,11 +9,4 @@ export const env = {
   },
   port: process.env.PORT ?? 8080,
   jwtSecret: process.env.JWT_SECRET ?? 'dsfasr21#$@#$sdpfokasdfop',
-  db: {
-    host: process.env.DB_HOST ?? 'localhost',
-    port: process.env.DB_PORT ?? 5432,
-    username: process.env.DB_USERNAME ?? 'postgres',
-    password: process.env.DB_PASSWORD ?? 'postgres',
-    database: process.env.DB_NAME ?? 'advanced_node_manguinho',
-  },
 };
--- src/main/server.ts
+++ src/main/server.ts
@@ -2,14 +2,12 @@ import './config/module-alias';
 
 import { createConnection } from 'typeorm';
 
-import { config } from '@/infra/postgres/helpers/config';
-
 import { app } from './config/app';
 import { env } from './config/env';
 
 import 'reflect-metadata';
 
-createConnection(config)
+createConnection()
   .then(() =>
     app.listen(env.port, () =>
       console.log(`Server running at http://localhost:${env.port}`),
