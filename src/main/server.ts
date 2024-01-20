9a6b515b8337a0ce2382cad296cc0f69b9512683
Luan Panno <luanpanno@gmail.com>
2024-01-20 01:51:25 -0300

    Connect to postgres db before server init

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,18 @@
 import './config/module-alias';
-import 'reflect-metadata';
+
+import { createConnection } from 'typeorm';
+
+import { config } from '@/infra/postgres/helpers/config';
 
 import { app } from './config/app';
 import { env } from './config/env';
 
-app.listen(env.port, () =>
-  console.log(`Server running at http://localhost:${env.port}`),
-);
+import 'reflect-metadata';
+
+createConnection(config)
+  .then(() =>
+    app.listen(env.port, () =>
+      console.log(`Server running at http://localhost:${env.port}`),
+    ),
+  )
+  .catch(console.error);
